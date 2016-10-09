d1d01879a9430cad858f3e5bad57253e120c6f45
pro100kot <pro100kot14@gmail.com>
2016-10-09 16:03:04 +0300

    few changes

--- app/src/avrdebug/communication/SimulAVRInitData.java
+++ app/src/avrdebug/communication/SimulAVRInitData.java
@@ -1,20 +1,20 @@
 package avrdebug.communication;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class SimulAVRInitData implements Serializable{
 	private static final long serialVersionUID = -2591376330837482355L;
-	private HashMap<String, LinkedHashMap<String, Boolean>> mcuVCDSources;
+	private HashMap<String, ArrayList<String>> mcuVCDSources;
 	
 	public SimulAVRInitData() {
 	}
-	public HashMap<String, LinkedHashMap<String, Boolean>> getMcuVCDSources() {
+	public HashMap<String, ArrayList<String>> getMcuVCDSources() {
 		return mcuVCDSources;
 	}
-	public void setMcuVCDSources(
-			HashMap<String, LinkedHashMap<String, Boolean>> mcuVCDSources) {
+	public void setMcuVCDSources(HashMap<String, ArrayList<String>> mcuVCDSources) {
 		this.mcuVCDSources = mcuVCDSources;
 	}
 }
