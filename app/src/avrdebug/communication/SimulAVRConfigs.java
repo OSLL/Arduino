56fa2bf0d64ee4e90211ac735f60e79e53d0ddb6
pro100kot <pro100kot14@gmail.com>
2016-10-09 15:30:42 +0300

    few changes

--- app/src/avrdebug/communication/SimulAVRConfigs.java
+++ app/src/avrdebug/communication/SimulAVRConfigs.java
@@ -6,12 +6,21 @@ import java.util.LinkedHashMap;
 
 public class SimulAVRConfigs implements Serializable {
 	private static final long serialVersionUID = -235225560474202938L;
+	/** List of all supported MCU*/
 	private ArrayList<String> mcuList;
+	/** Current selected MCU*/
+	private String selectedMcu;
+	/** MCU's CPU frequency*/
 	private long cpuFreq;
+	/** If true - simulator provides tracing AVR core*/
 	private boolean isTraceEnable;
+	/** If true - gdb-server will be started on debug server*/
 	private boolean isDebugEnable;
+	/** Maximum simulator run time. 0 or -1 if option is not required*/
 	private long maxRunTime;
+	/** If true - simulator provides VCD trace dump*/
 	private boolean isVCDTraceEnable;
+	/** List of available VCD-trace sources with flags (flag==true if source should be traced) */
 	private LinkedHashMap<String, Boolean> vcdSources;
 	
 	public SimulAVRConfigs() {
@@ -22,6 +31,13 @@ public class SimulAVRConfigs implements Serializable {
 	}
 	public void setMcuList(ArrayList<String> mcuList) {
 		this.mcuList = mcuList;
+	}
+	public String getSelectedMcu() {
+		return selectedMcu;
+	}
+
+	public void setSelectedMcu(String selectedMcu) {
+		this.selectedMcu = selectedMcu;
 	}
 	public long getCpuFreq() {
 		return cpuFreq;
